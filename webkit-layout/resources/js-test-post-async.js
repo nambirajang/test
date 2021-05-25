ac84e97908cccf4ad0177d0dafbe211faf982e1f
nrajan002c <nambi_rajang@cable.comcast.com>
2021-05-25 03:37:04 -0400

    sdfih 324
    Signed-off-by: nrajan002c <nambi_rajang@cable.comcast.com>

---
+++ webkit-layout/resources/js-test-post-async.js
@@ -0,0 +1,7 @@
+if (!errorMessage)
+    successfullyParsed = true;
+shouldBeTrue("successfullyParsed");
+debug('<br /><span class="pass">TEST NAMBICOMPLETE</span>');
+
+if (window.testRunner)
+    testRunner.notifyDone();
